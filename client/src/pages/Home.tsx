1475b22ea26b296b0892b785dfbfafcb0181dafb
hassanniaxi <hassanniazi329@gmail.com>
2024-12-19 22:51:40 +0500

    Added_tailwind

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="text-red-600 text-9xl text-[100px]">Home Page hello</h1>
+
+      <p> kmewjcfuewiucfne vhjrvbrehjv </p>
     </div>
   );
 }
