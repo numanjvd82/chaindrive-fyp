902d93d71e86b1ae4252a775aa507eff18b8f140
hassanniaxi <hassanniazi329@gmail.com>
2024-12-19 23:09:14 +0500

    Testing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-600 text-9xl text-[100px]">Home Page hello</h1>
+      <h1 className="text-red-600 text-9xl text-[100px]">Ameer Hassan Khan</h1>
 
       <p> kmewjcfuewiucfne vhjrvbrehjv </p>
     </div>
