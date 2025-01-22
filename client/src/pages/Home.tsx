d3b85e4b19e05bedf801e25e4c80bb9489889d79
hassanniaxi <hassanniazi329@gmail.com>
2025-01-22 23:32:48 +0500

    added_files

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,26 +1,41 @@
-import { LogoutButton } from "../components/LogoutButton";
-import { useUser } from "../hooks/useUser";
+import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import HowItWork from "../components/HowItWork";
+import MainSearch from "../components/MainSearch";
+import Navbar from "../components/Navbar";
+import PopularRenterDeals from "../components/PopularRenterDeals";
+import WhyChooseUs from "../components/WhyChooseUs";
+
+const handleSearch = (location: string, pickupDate: string, returnDate: string) => {
+  console.log("Search Details:", { location, pickupDate, returnDate });
+};
+
 
 export default function Home() {
-  const { user } = useUser();
+  // const { user } = useUser();
 
-  if (!user) {
-    return (
-      <h1 className="text-red-600 text-9xl text-[100px]">
-        You are not logged in
-      </h1>
-    );
-  }
+  // if (!user) {
+  //   return (
+  //     <h1 className="text-red-600 text-9xl text-[100px]">
+  //       You are not logged in
+  //     </h1>
+  //   );
+  // }
 
   return (
     <div>
-      <h1 className="text-red-600 text-9xl text-[100px]">
-        This is a home page
-      </h1>
 
-      <LogoutButton />
+    <Navbar />,
+    <HeroSection />
+    <MainSearch onSearch={handleSearch}/>
+    <HowItWork />
+    <WhyChooseUs />
+    <PopularRenterDeals />
+    <Footer />
 
-      <p>{user.role === "owner" ? "You are an owner" : "You are a renter"}</p>
+      {/* <LogoutButton /> */}
+
+      {/* <p>{user.role === "owner" ? "You are an owner" : "You are a renter"}</p> */}
     </div>
   );
 }
